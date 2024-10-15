bdc0283ca554cc7266e4f36651ddb81beb677004
kamalika0363 <kamalikaghora14@gmail.com>
2024-10-15 14:20:50 +0530

    'sidebar'

---
+++ app/admin/dashboard/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function orders() {
+    return (
+        <div>
+            <h1>Orders</h1>
+        </div>
+    );
+}
---
+++ app/admin/dashboard/products/page.tsx
@@ -0,0 +1,7 @@
+export default function products(){
+    return (
+        <div>
+        <h1>Products</h1>
+        </div>
+    );
+}
