5a4d4aeb256e6d438102a5785b288b61f82777ba
kamalika0363 <kamalikaghora14@gmail.com>
2024-10-24 00:56:28 +0530

    skeleton

--- app/franchisee/[id]/error.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-export default function Error() {
-    return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-        </div>
-    )
-}
--- app/franchisee/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return(
-        <div className="flex items-center justify-center">
-            <div className="text-2xl">Loading...</div>
-        </div>
-    )
-}
